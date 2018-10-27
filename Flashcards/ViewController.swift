f4d00c8627c498d37f09ac54757642b1a04b7985
coraxyc <ycxing99@gmail.com>
2018-10-26 17:45:13 -0700

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Cora Xing on 10/26/18.
+//  Copyright © 2018 Cora Yichen Xing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
