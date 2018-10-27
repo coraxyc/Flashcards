e3d584f33df1fbed06b33072b614ea21ee255b78
coraxyc <ycxing99@gmail.com>
2018-10-26 23:31:23 -0700

    Rounded buttons and added multiple choice selection with reset

--- src/Flashcards/ViewController.swift
+++ src/Flashcards/ViewController.swift
@@ -12,20 +12,73 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet weak var card: UIView!
+    @IBOutlet weak var btnOptionOne: UIButton!
+    @IBOutlet weak var btnOptionTwo: UIButton!
+    @IBOutlet weak var btnOptionThree: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        // Change appearance of labels
+        frontLabel.layer.cornerRadius = 30.0 // create rounded card
+        frontLabel.clipsToBounds = true // clip front label inside
+        backLabel.layer.cornerRadius = 30.0 // create rounded card
+        backLabel.clipsToBounds = true // clip front label inside
+        
+        // Change appearance of cards
+        card.layer.cornerRadius = 30.0 // create rounded card
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.2
+        
+        // Change appearance of buttons
+        
+        // Button 1
+        btnOptionOne.layer.cornerRadius = 15.0
+        btnOptionOne.layer.borderWidth = 3
+        btnOptionOne.layer.borderColor = #colorLiteral(red: 0.2790087163, green: 0.09912771732, blue: 0.324397862, alpha: 1)
+        
+        // Button 2
+        btnOptionTwo.layer.cornerRadius = 15.0
+        btnOptionTwo.layer.borderWidth = 3
+        btnOptionTwo.layer.borderColor = #colorLiteral(red: 0.2790087163, green: 0.09912771732, blue: 0.324397862, alpha: 1)
+        
+        // Button 3
+        btnOptionThree.layer.cornerRadius = 15.0
+        btnOptionThree.layer.borderWidth = 3
+        btnOptionThree.layer.borderColor = #colorLiteral(red: 0.2790087163, green: 0.09912771732, blue: 0.324397862, alpha: 1)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         frontLabel.isHidden = !frontLabel.isHidden
         print("test")
+    }
+    
+    @IBAction func didTapOptionOne(_ sender: Any) {
+        btnOptionOne.isHidden = true
+    }
+    
+    @IBAction func didTapOptionTwo(_ sender: Any) {
+        frontLabel.isHidden = true
+    }
+    
+    @IBAction func didTapOptionThree(_ sender: Any) {
+        btnOptionThree.isHidden = true
+    }
+    
+    @IBAction func didTapOutside(_ sender: Any) {
+        // Resets flashcard settings
+        frontLabel.isHidden = false
+        btnOptionOne.isHidden = false
+        btnOptionTwo.isHidden = false
+        btnOptionThree.isHidden = false
+        
     }
 }
 
