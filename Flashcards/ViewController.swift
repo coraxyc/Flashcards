46395418d29cb7e1d698a61d959bd8beb9949ee5
coraxyc <ycxing99@gmail.com>
2018-10-26 18:31:33 -0700

    Initial commit with one flashcard

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var frontLabel:UILabel!
+    @IBOutlet weak var backLabel:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+    
+    }
 }
 
